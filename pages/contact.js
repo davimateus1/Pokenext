43399cd46e4dd06541f7258c7e107d0750ad3181
Davi Mateus <davi.2020109493@unicap.br>
2022-01-25 01:04:17 -0300

    contact page finished

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,26 @@
-export default function contact() {
-    return (
-        <div>
-            <h1>Contato</h1>
-        </div>
-    )
+import { BsWhatsapp, BsLinkedin, BsInstagram } from "react-icons/bs";
+import { SiGmail } from "react-icons/si";
+
+import styles from "../styles/Contact.module.css";
+
+function Contact() {
+  return (
+    <section className={styles.contact_container}>
+        <h1>Contato</h1>
+      <p>
+        <SiGmail /> <a href="mailto:davimateusga@gmail.com"> davimateusga@gmail.com </a>
+      </p>
+      <p>
+        <BsWhatsapp /> <a href="https://api.whatsapp.com/send?phone=5587988476864&text=Enviar%20mensagem%20para%20Davi%20Mateus" target="_blank" rel="noreferrer"> (87) 98847-6864 </a>
+      </p>
+      <p>
+        <BsInstagram /> <a href="https://www.instagram.com/davimateus1" target="_blank" rel="noreferrer"> @davimateus1 </a>
+      </p>
+      <p>
+        <BsLinkedin /> <a href="https://www.linkedin.com/in/davimateusg/" target="_blank" rel="noreferrer"> /davimateusg </a>
+      </p>
+    </section>
+  );
 }
+
+export default Contact;
